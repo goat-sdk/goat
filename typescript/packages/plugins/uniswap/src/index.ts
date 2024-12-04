cb023552a4f1a8f327caf69e359a31db533ba154
dannydaniil <danny.daniil@uniswap.org>
2024-12-03 22:44:40 -0500

    define method

--- typescript/packages/plugins/uniswap/src/index.ts
+++ typescript/packages/plugins/uniswap/src/index.ts
@@ -1,26 +1,30 @@
 import type { Chain, EVMWalletClient, Plugin } from "@goat-sdk/core";
-import { getTools } from "./tools";
 import { mainnet } from "viem/chains";
-import { type ApiKeyCredentials, createOrder, createOrDeriveAPIKey } from "./api";
+import {
+	type ApiKeyCredentials,
+	createOrDeriveAPIKey,
+	createOrder,
+} from "./api";
+import { getTools } from "./tools";
 
 export type UniswapOptions = {
-    credentials: ApiKeyCredentials;
+	credentials: ApiKeyCredentials;
 };
 
 export function uniswap({
-    credentials,
+	credentials,
 }: UniswapOptions): Plugin<EVMWalletClient> {
-    return {
-        name: "Uniswap",
-        supportsChain: (chain: Chain) =>
-            chain.type === "evm" && chain.id === mainnet.id,
-        supportsSmartWallets: () => false,
-        getTools: async () => {
-            return getTools({
-                credentials,
-            });
-        },
-    };
+	return {
+		name: "Uniswap",
+		supportsChain: (chain: Chain) =>
+			chain.type === "evm" && chain.id === mainnet.id,
+		supportsSmartWallets: () => false,
+		getTools: async () => {
+			return getTools({
+				credentials,
+			});
+		},
+	};
 }
 
 export { createOrDeriveAPIKey, createOrder };
