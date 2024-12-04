4a839d4a1dd6afe694daf85403af9fa759bcc167
dannydaniil <danny.daniil@uniswap.org>
2024-12-03 22:56:37 -0500

    clean up api key and base url

--- typescript/packages/plugins/uniswap/src/api.ts
+++ typescript/packages/plugins/uniswap/src/api.ts
@@ -13,14 +13,16 @@ function getApiKey(): string {
 export async function getQuote(
 	parameters: z.infer<typeof getQuoteBodySchema>,
 	// biome-ignore lint/suspicious/noExplicitAny: Need to create a schema for the response
+	apiKey: string,
+	baseUrl: string,
 ): Promise<any> {
-	const url = new URL(`${getBaseUrl()}/quote`);
+	const url = new URL(`${baseUrl}/quote`);
 
 	const response = await fetch(url.toString(), {
 		method: "POST",
 		body: JSON.stringify(parameters),
 		headers: {
-			"x-api-key": getApiKey(),
+			"x-api-key": apiKey,
 		},
 	});
 	if (!response.ok) {
