bdc51be4d606533484f7dd85744ca89a50a761b5
dannydaniil <danny.daniil@uniswap.org>
2024-12-03 23:03:13 -0500

    add swap too

--- typescript/packages/plugins/uniswap/src/api.ts
+++ typescript/packages/plugins/uniswap/src/api.ts
@@ -1,14 +1,5 @@
 import type { z } from "zod";
-import type { getQuoteBodySchema } from "./parameters";
-import { mustBeDefined } from "./utils";
-
-function getBaseUrl(): string {
-	return mustBeDefined(process.env.UNISWAP_BASE_URL);
-}
-
-function getApiKey(): string {
-	return mustBeDefined(process.env.UNISWAP_API_KEY);
-}
+import type { getQuoteBodySchema, getSwapBodySchema } from "./parameters";
 
 export async function getQuote(
 	parameters: z.infer<typeof getQuoteBodySchema>,
@@ -27,6 +18,28 @@ export async function getQuote(
 	});
 	if (!response.ok) {
 		throw new Error(`Failed to fetch quote: ${response.statusText}`);
+	}
+
+	return await response.json();
+}
+
+export async function getSwap(
+	parameters: z.infer<typeof getSwapBodySchema>,
+	// biome-ignore lint/suspicious/noExplicitAny: Need to create a schema for the response
+	apiKey: string,
+	baseUrl: string,
+): Promise<any> {
+	const url = new URL(`${baseUrl}/swap`);
+
+	const response = await fetch(url.toString(), {
+		method: "POST",
+		body: JSON.stringify(parameters),
+		headers: {
+			"x-api-key": apiKey,
+		},
+	});
+	if (!response.ok) {
+		throw new Error(`Failed to fetch swap: ${response.statusText}`);
 	}
 
 	return await response.json();
