3a5d826bd418aa8c5d4fea18297828d3b674a0cc
dannydaniil <danny.daniil@uniswap.org>
2024-12-03 22:44:40 -0500

    define method

--- typescript/packages/plugins/uniswap/src/parameters.ts
+++ typescript/packages/plugins/uniswap/src/parameters.ts
@@ -1,4 +1,16 @@
 import { z } from "zod";
 
+enum SwapType {
+	EXACT_IN = "EXACT_IN",
+	EXACT_OUT = "EXACT_OUT",
+}
+
 export const getQuoteParametersSchema = z.object({
+	tokenIn: z.string(),
+	tokenOut: z.string(),
+	amount: z.string(),
+	tokenInChainId: z.number(),
+	tokenOutChainId: z.number(),
+	swapper: z.string(),
+	type: z.nativeEnum(SwapType),
 });
--- typescript/packages/plugins/uniswap/src/tools.ts
+++ typescript/packages/plugins/uniswap/src/tools.ts
@@ -2,33 +2,28 @@ import type { DeferredTool, EVMWalletClient } from "@goat-sdk/core";
 import type { z } from "zod";
 
 import type { ApiKeyCredentials } from "./api";
-import {
-    getQuoteParametersSchema,
-} from "./parameters";
+import { getQuoteParametersSchema } from "./parameters";
 
-import {
-    getEvents,
-} from "./api";
+import { getQuote } from "./api";
 
-export type PolymarketToolsOptions = {
-    credentials: ApiKeyCredentials;
+export type UniswapToolsOptions = {
+	credentials: ApiKeyCredentials;
 };
 
 export function getTools({
-    credentials,
-}: PolymarketToolsOptions): DeferredTool<EVMWalletClient>[] {
-    return [
-        {
-            name: "get_uniswap_quote",
-            description:
-                "This {{tool}} gets the quotei for a Uniswap swap",
-            parameters: getQuoteParametersSchema,
-            method: async (
-                walletClient: EVMWalletClient,
-                parameters: z.infer<typeof getQuoteParametersSchema>
-            ) => {
-                return getEvents(parameters);
-            },
-        },
-    ];
+	credentials,
+}: UniswapToolsOptions): DeferredTool<EVMWalletClient>[] {
+	return [
+		{
+			name: "get_uniswap_quote",
+			description: "This {{tool}} gets the quotei for a Uniswap swap",
+			parameters: getQuoteParametersSchema,
+			method: async (
+				walletClient: EVMWalletClient,
+				parameters: z.infer<typeof getQuoteParametersSchema>,
+			) => {
+				return getQuote(parameters);
+			},
+		},
+	];
 }
