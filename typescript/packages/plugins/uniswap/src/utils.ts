8d7d83d2c8e0e6533301b6923c437efb04d0ac57
dannythedawger <daniel.c.daniil@gmail.com>
2024-12-07 12:43:36 -0500

    pubish guide

--- typescript/packages/plugins/uniswap/src/utils.ts
+++
@@ -1,6 +0,0 @@
-export function mustBeDefined<T>(value: T | null | undefined, message = "Should be defined"): T {
-    if (value === null || value === undefined) {
-        throw new Error(message);
-    }
-    return value;
-}
