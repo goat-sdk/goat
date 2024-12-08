444e6a553ee25d50e926b53b952b96894091547e
dannythedawger <daniel.c.daniil@gmail.com>
2024-12-08 16:19:31 -0500

    more liniting

--- typescript/examples/vercel-ai/coingecko/index.ts
+++ typescript/examples/vercel-ai/coingecko/index.ts
@@ -18,9 +18,9 @@ require("dotenv").config();
 const account = privateKeyToAccount(process.env.WALLET_PRIVATE_KEY as `0x${string}`);
 
 const walletClient = createWalletClient({
-	account: account,
-	transport: http(process.env.RPC_PROVIDER_URL),
-	chain: sepolia,
+    account: account,
+    transport: http(process.env.RPC_PROVIDER_URL),
+    chain: sepolia,
 });
 
 (async () => {
