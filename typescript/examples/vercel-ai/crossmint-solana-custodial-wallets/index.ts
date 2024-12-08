ff8b9eaebba6b2f131561b45866d1c9b6426955f
Bryan Eastwood <bryan@paella.dev>
2024-12-08 14:57:41 -0500

    changes

--- typescript/examples/vercel-ai/crossmint-solana-custodial-wallets/index.ts
+++ typescript/examples/vercel-ai/crossmint-solana-custodial-wallets/index.ts
@@ -4,7 +4,6 @@ import { generateText } from "ai";
 import { getOnChainTools } from "@goat-sdk/adapter-vercel-ai";
 import { crossmint } from "@goat-sdk/crossmint";
 import { Connection } from "@solana/web3.js";
-import { nfts } from "@goat-sdk/plugin-solana-actions";
 
 require("dotenv").config();
 
@@ -16,24 +15,22 @@ if (!apiKey || !email) {
 }
 
 const { custodial } = crossmint(apiKey);
-const connection = new Connection("https://api.devnet.solana.com", "confirmed");
 
 (async () => {
     const tools = await getOnChainTools({
         wallet: await custodial({
             chain: "solana",
-            address: "3q1PB3Yde3wUnJ45RLrAhCg3W1n9HEC8Jd9NkfuhRGS1",
+            email: email,
             env: "staging",
-            connection,
+            connection: new Connection("https://api.devnet.solana.com", "confirmed"),
         }),
-        plugins: [nfts(connection)],
     });
 
     const result = await generateText({
         model: openai("gpt-4o-mini"),
         tools: tools,
         maxSteps: 5,
-        prompt: "Transfer NFT with assetId 57hD2akP2FsEwnDMJn8CNS565p2dNUz8UKuQ5tUGEsh7 to the wallet CKkeuMRjRsM9zBNnFVRRYhQ3rg4uL4tPkaADNLgk2omb",
+        prompt: "Get my balance in SOL",
     });
 
     console.log(result.text);
