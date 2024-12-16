51bee99554b690d09a61e6d13ceb25d98d5561ad
mPaella <93682696+mPaella@users.noreply.github.com>
2024-12-16 13:34:29 -0500

    v2: dont add new v2 package (#77)
    dont add new v2 package

--- typescript/packages/wallets/evm/src/EVMWalletClient.ts
+++ typescript/packages/wallets/evm/src/EVMWalletClient.ts
@@ -1,4 +1,4 @@
-import { type EvmChain, type Signature, WalletClientBase } from "@goat-sdk/core-v2";
+import { type EvmChain, type Signature, WalletClientBase } from "@goat-sdk/core";
 import type { EVMReadRequest, EVMReadResult, EVMTransaction, EVMTypedData } from "./types";
 
 export abstract class EVMWalletClient extends WalletClientBase {
