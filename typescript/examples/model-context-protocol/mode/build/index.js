3402b91cc1a48e60a8af58e27e192bf9f82f309a
Agustin Armellini Fischer <armellini13@gmail.com>
2024-12-20 17:07:44 +0100

    Support MCP

---
+++ typescript/examples/model-context-protocol/mode/build/index.js
@@ -0,0 +1,54 @@
+import { Server } from "@modelcontextprotocol/sdk/server/index.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { CallToolRequestSchema, ListToolsRequestSchema, } from "@modelcontextprotocol/sdk/types.js";
+import { privateKeyToAccount } from "viem/accounts";
+import { createWalletClient, http } from "viem";
+import { mode } from "viem/chains";
+import { MODE, USDC, erc20 } from "@goat-sdk/plugin-erc20";
+import { kim } from "@goat-sdk/plugin-kim";
+import { sendETH } from "@goat-sdk/wallet-evm";
+import { viem } from "@goat-sdk/wallet-viem";
+import { getOnChainTools } from "@goat-sdk/adapter-model-context-protocol";
+const server = new Server({
+    name: "goat",
+    version: "1.0.0",
+}, {
+    capabilities: {
+        tools: {},
+    },
+});
+const account = privateKeyToAccount(process.env.WALLET_PRIVATE_KEY);
+const walletClient = createWalletClient({
+    account: account,
+    transport: http(process.env.RPC_PROVIDER_URL),
+    chain: mode,
+});
+// Initialize tools once
+const toolsPromise = getOnChainTools({
+    wallet: viem(walletClient),
+    plugins: [sendETH(), erc20({ tokens: [USDC, MODE] }), kim()],
+});
+server.setRequestHandler(ListToolsRequestSchema, async () => {
+    const { listOfTools } = await toolsPromise;
+    return {
+        tools: listOfTools(),
+    };
+});
+server.setRequestHandler(CallToolRequestSchema, async (request) => {
+    const { toolHandler } = await toolsPromise;
+    try {
+        return toolHandler(request.params.name, request.params.arguments);
+    }
+    catch (error) {
+        throw new Error(`Tool ${name} failed: ${error}`);
+    }
+});
+async function main() {
+    const transport = new StdioServerTransport();
+    await server.connect(transport);
+    console.error("GOAT MCP Server running on stdio");
+}
+main().catch((error) => {
+    console.error("Fatal error in main():", error);
+    process.exit(1);
+});
