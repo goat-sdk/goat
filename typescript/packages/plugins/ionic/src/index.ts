7827c69010da8b085aef203ded4c54afdd68fffb
Arunkumar v <arunkumarv1530@gmail.com>
2024-12-24 21:09:08 +0530

    ionic

--- typescript/packages/plugins/ionic/src/index.ts
+++ typescript/packages/plugins/ionic/src/index.ts
@@ -1,5 +1,5 @@
+// typescript/packages/plugins/ionic/src/index.ts
 export * from "./config";
 export * from "./types";
-export { IonicPlugin, ionic } from "./ionic.plugin";
-export { IonicTools } from "./ionic.service";
-export type { SupplyAssetParams, BorrowAssetParams, GetHealthMetricsParams } from "./ionic.service";
+export { IonicPlugin, ionic } from "./ionic.plugin"; // Explicitly re-export from ionic.plugin.ts
+export { IonicTools } from "./ionic.service";       // Explicitly re-export from ionic.service.ts
--- typescript/packages/plugins/ionic/src/ionic.plugin.ts
+++ typescript/packages/plugins/ionic/src/ionic.plugin.ts
@@ -1,19 +1,20 @@
+// typescript/packages/plugins/ionic/src/ionic.plugin.ts
 import { type Chain, PluginBase } from "@goat-sdk/core";
 import { EVMWalletClient } from "@goat-sdk/wallet-evm";
-import { IonicTools } from "./ionic.service";
+import { IonicTools } from "./ionic.service"; // Correct import: IonicTools
 
 export interface IonicPluginOptions {
-    supportedTokens?: string[]; // Optional list of tokens to support (can be used for filtering or UI hints)
+    supportedTokens?: string[]; // Optional list of tokens to support
 }
 
 export class IonicPlugin extends PluginBase<EVMWalletClient> {
-    constructor(options?: IonicPluginOptions) {
-        super("ionic", [new IonicTools()]);
+    constructor(options?: IonicPluginOptions) { // Accept options in constructor
+        super("ionic", [new IonicTools()]); // Correct instantiation: IonicTools
     }
 
     supportsChain = (chain: Chain) => chain.type === "evm";
 }
 
-export function ionic(options?: IonicPluginOptions) {
-    return new IonicPlugin(options);
+export function ionic(options?: IonicPluginOptions) { // Make options optional here as well if needed
+    return new IonicPlugin(options); // Pass options to the constructor
 }
