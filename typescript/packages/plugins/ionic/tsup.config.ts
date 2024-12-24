4d6f833d7f2d49b349bf975e9ae52fc1398bc629
Arunkumar v <arunkumarv1530@gmail.com>
2024-12-24 18:16:51 +0530

    ionic

--- typescript/packages/plugins/ionic/tsup.config.ts
+++ typescript/packages/plugins/ionic/tsup.config.ts
@@ -2,27 +2,11 @@ import { defineConfig } from "tsup";
 import { treeShakableConfig } from "../../../tsup.config.base";
 
 export default defineConfig({
-  ...treeShakableConfig,
-  
-  entry: [
-    "src/index.ts",
-    "src/ionic.plugin.ts",
-    "src/ionic.service.ts",
-    "src/types.ts",
-    "src/abi.ts"
-  ],
-  
-  dts: true,
-
-  splitting: true,
- 
-  sourcemap: true,
-
-  external: [
-    "@goat-sdk/core",
-    "@goat-sdk/wallet-evm",
-    "viem",
-    "zod"
-  ],
-  
+    ...treeShakableConfig,
+    external: [],
+    treeshake: true,
+    onSuccess: "tsc --project tsconfig.dts.json",
+    loader: {
+        '.json': 'json'
+    }
 });
