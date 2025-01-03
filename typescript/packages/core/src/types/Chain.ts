1228bb3dd19b260cb9f83dc3ce1de0a5761b4a83
Richard Watts <richard@zilliqa.com>
2025-01-03 12:52:02 +0000

    (feat) Add a chain type, wallet and plugin for the Zilliqa blockchain

--- typescript/packages/core/src/types/Chain.ts
+++ typescript/packages/core/src/types/Chain.ts
@@ -2,7 +2,7 @@
  * @param type - "evm" or "solana", extend this union as needed (e.g., "sui")
  * @param id - Chain ID, optional for EVM
  */
-export type Chain = EvmChain | SolanaChain | AptosChain | ChromiaChain | FuelChain;
+export type Chain = EvmChain | SolanaChain | AptosChain | ChromiaChain | FuelChain | ZilliqaChain;
 
 export type EvmChain = {
     type: "evm";
@@ -23,4 +23,10 @@ export type ChromiaChain = {
 
 export type FuelChain = {
     type: "fuel";
+};
+
+export type ZilliqaChain = {
+    type: "zilliqa";
+    id: number;
+    evmId: number;
 };
