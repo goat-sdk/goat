a3021e49e0d6c19ce5a603cd766eab163b3f1b9d
Mahmoud Mohajer <mahmohajer3@gmail.com>
2025-01-08 23:26:41 +0430

    adding starknet as chain

--- typescript/packages/core/src/types/Chain.ts
+++ typescript/packages/core/src/types/Chain.ts
@@ -2,7 +2,7 @@
  * @param type - "evm" or "solana", extend this union as needed (e.g., "sui")
  * @param id - Chain ID, optional for EVM
  */
-export type Chain = EvmChain | SolanaChain | AptosChain | ChromiaChain | FuelChain;
+export type Chain = EvmChain | SolanaChain | AptosChain | ChromiaChain | FuelChain | StarknetChain;
 
 export type EvmChain = {
     type: "evm";
@@ -23,4 +23,8 @@ export type ChromiaChain = {
 
 export type FuelChain = {
     type: "fuel";
+};
+
+export type StarknetChain = {
+    type: "starknet";
 };
