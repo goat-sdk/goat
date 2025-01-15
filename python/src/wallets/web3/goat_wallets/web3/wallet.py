190d7d5ca602f14e620dff4ff2d01fedf50dc890
Agus <armellini13@gmail.com>
2025-01-15 15:49:59 +0100

    Update wallet.py

--- python/src/wallets/web3/goat_wallets/web3/wallet.py
+++ python/src/wallets/web3/goat_wallets/web3/wallet.py
@@ -106,7 +106,7 @@ class Web3EVMWalletClient(EVMWalletClient):
                 tx_params["paymasterInput"] = paymaster_input
 
             tx_hash = self._web3.eth.send_transaction(tx_params)
-            return self._wait_for_receipt(HexStr(tx_hash.hex()))
+            return self._wait_for_receipt(HexStr(tx_hash.to_0x_hex()))
 
         # Contract call
         function_name = transaction.get("functionName")
@@ -134,7 +134,7 @@ class Web3EVMWalletClient(EVMWalletClient):
         # Send the transaction
         tx_hash = contract_function(*args).transact(tx_params)
 
-        return self._wait_for_receipt(HexStr(tx_hash.hex()))
+        return self._wait_for_receipt(HexStr(tx_hash.to_0x_hex()))
 
     def read(self, request: EVMReadRequest) -> EVMReadResult:
         """Read data from a smart contract."""
