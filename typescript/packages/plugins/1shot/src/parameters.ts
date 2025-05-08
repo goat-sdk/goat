cc6f034df89e8f52fd7731ac1e644c07ae001ec9
Charlie Sibbach <charlie@uxly.software>
2025-05-08 15:07:17 -0700

    Linting

--- typescript/packages/plugins/1shot/src/parameters.ts
+++ typescript/packages/plugins/1shot/src/parameters.ts
@@ -1,13 +1,8 @@
 import { createToolParameters } from "@goat-sdk/core";
-import { z } from "zod";
-import { listEscrowWalletsSchema, listTransactionsSchema, createTransactionSchema } from "@uxly/1shot-client";
+import { createTransactionSchema, listEscrowWalletsSchema, listTransactionsSchema } from "@uxly/1shot-client";
 
 export class ListTransactionsParams extends createToolParameters(listTransactionsSchema) {}
 
-export class ListEscrowWalletsParams extends createToolParameters(
-    listEscrowWalletsSchema
-) {}
+export class ListEscrowWalletsParams extends createToolParameters(listEscrowWalletsSchema) {}
 
 export class CreateTransactionParams extends createToolParameters(createTransactionSchema) {}
-
-
